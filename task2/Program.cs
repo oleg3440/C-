84528c2bdb1196e4b4b0b9428b6a433143c0538c
yegor3452 <ntsirius@yandex.ru>
2022-10-07 15:21:40 +0500

    task8

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+
+if (b > max) max = b;
+if (c > max ) max = c;
+    Console.WriteLine (max);
---
+++ task3/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+if (a % 2 == 0)
+    Console.Write("четна");
+else    
+    Console.Write("нечетна");
+
+

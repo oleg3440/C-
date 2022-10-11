1dc1812bcc5be2ebf1aa29f3870c93c7375da13e
yegor3452 <ntsirius@yandex.ru>
2022-10-11 19:02:35 +0500

    191

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,23 +1,28 @@
 ﻿int a = new Random().Next(10000, 100000);
 Console.Write($"{a}\n");
 
-int b1 = a % 10;
+int b = a % 10;
+int c = a / 10 % 10;
+int d = a / 1000 % 10;
+int e = a / 10000;
+Console.Write($"{b} {c} {d} {e}");
 
-if  (a < 100)
-    Console.Write("третьей цифры нет");
-else 
-    Console.Write(b
+
+//if  (a < 100)
+ //   Console.Write("третьей цифры нет");
+//else 
+    //Console.Write(b
     
 
 
 
 
-int a = new Random().Next(10000, 100000);
-int.TryParse(Console.ReadLine()!, out a);
+//int a = new Random().Next(10000, 100000);
+//int.TryParse(Console.ReadLine()!, out a);
 
-string s = a.ToString();
+//string s = a.ToString();
 
-if (s.Length < 3)
-    Console.Write($"Третьей цифры нет");
-else
-    Console.Write($"{s[2]}");
+//if (s.Length < 3)
+//    Console.Write($"Третьей цифры нет");
+//else
+//    Console.Write($"{s[2]}");

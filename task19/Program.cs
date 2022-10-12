a876373cc54081ec0a747b72d8c36e373829112f
yegor3452 <ntsirius@yandex.ru>
2022-10-12 17:06:45 +0500

    задача 19 решена

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,28 +1,13 @@
-﻿int a = new Random().Next(10000, 100000);
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int a;
+int.TryParse(Console.ReadLine()!, out a);
 Console.Write($"{a}\n");
-
 int b = a % 10;
 int c = a / 10 % 10;
 int d = a / 1000 % 10;
 int e = a / 10000;
-Console.Write($"{b} {c} {d} {e}");
+if (b==e && c==d)
+Console.WriteLine ("число является полиандром");
+else
+Console.WriteLine ("число не является полиандром");
 
-
-//if  (a < 100)
- //   Console.Write("третьей цифры нет");
-//else 
-    //Console.Write(b
-    
-
-
-
-
-//int a = new Random().Next(10000, 100000);
-//int.TryParse(Console.ReadLine()!, out a);
-
-//string s = a.ToString();
-
-//if (s.Length < 3)
-//    Console.Write($"Третьей цифры нет");
-//else
-//    Console.Write($"{s[2]}");

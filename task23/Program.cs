875b161bca6855bcb5ae655fd236708b818b25a4
yegor3452 <ntsirius@yandex.ru>
2022-10-13 18:21:12 +0500

    23

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,31 +1,9 @@
 ﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
-//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
-//4  -> 24  5  -> 120 
 int N;
 Console.Write($"Введите число: ");
 int.TryParse(Console.ReadLine()!, out N);
-int cub = 1;
-int c = cub;
-
-Console.Write($"{c}");
-
+for (int i = 1; i <= N; i++)
 {
- for (int i = 1; i <= N; i++);
-    c = cub * cub * cub;
-    return c;
+    Console.WriteLine(i*i*i);
 }
-
-
-
-
-//int Factorial(int a)
-//{
-  //  int f = 1;
-   // for (int i = 2; i <= a; i++)
-   //     f = f * i;
-  //  return f;
-//}
-
-
-

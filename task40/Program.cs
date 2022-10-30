f27f54cfe4dc75164b320147487f08a230c34661
yegor3452 <ntsirius@yandex.ru>
2022-10-30 19:02:03 +0500

    task40

---
+++ task40/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 40: Напишите программу, которая принимает на вход три числа и 
+//проверяет, может ли существовать треугольник с сторонами такой длины.            */
+
+int length = new Random().Next(3, 3);
+int[] array = new int[length];
+for (int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+
+int a = array[0];
+int b = array[1];
+int c = array[2];
+
+
+if (a + b > c && a + c > b && b + c > a)
+    Console.Write($"Такой треугольник существует");
+else
+    Console.Write($"Такого треугольника не существует");

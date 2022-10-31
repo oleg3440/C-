e4653f22e47ef1bfca1d2c89d592ff42e027cbf6
yegor3452 <ntsirius@yandex.ru>
2022-10-31 20:40:55 +0500

    task45

---
+++ task45/Program.cs
@@ -0,0 +1,17 @@
+﻿// Создайте программу, котрая будет принимать на вход элементы, которые
+// будут записываться в массив, пока Пользователь не введет 0.
+List<int> array = new List<int>();
+
+int a;
+int.TryParse(Console.ReadLine()!, out a);
+
+while (a !=0)
+{
+    array.Add(a);
+    int.TryParse(Console.ReadLine()!, out a);
+}
+
+for (int i = 0; i < array.Count; i++)
+{
+    Console.Write($"{array[i]} ");
+}

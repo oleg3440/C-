bb5a5ac1d7ba484f0fe7f781bdec53137f3fe8ea
yegor3452 <ntsirius@yandex.ru>
2022-10-31 10:30:52 +0500

    task42

--- task42/Program.cs
+++ task42/Program.cs
@@ -22,3 +22,30 @@ while (temp > 0)
 
 Console.Write(str);
  
+ /* **Задача 42:** 2-е решение Напишите программу, которая будет 
+ преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10 */
+
+int a;
+Console.Write($"Введите число для перевода: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+int b;
+Console.Write($"Введите систему: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+if (b > 16)
+b = 10;
+
+int temp = a;
+string str = "";
+
+string nums = "0123456789ABCDEF";
+
+while (temp > 0)
+{
+str = str.Insert(0, (nums[temp % b]).ToString());
+temp /= b;
+}

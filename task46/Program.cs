382667b6245fbc759d6095503098c8e5a7f2d71d
yegor3452 <ntsirius@yandex.ru>
2022-10-31 19:22:33 +0500

    task46

---
+++ task46/Program.cs
@@ -0,0 +1,35 @@
+﻿// **Задача 46 ** Задвйте двумерный массив mxn, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 - 2 33 - 2
+// 77 3 8 1
+
+int[,] array = new int [3, 4];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = new Random().Next(10);
+    }
+}
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        Console.Write($"{array[i, j]}  ");
+    }
+    Console.WriteLine();
+}
+
+//второй вариант сокращенный
+int[,] array = new int [3, 4];
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]}  ");
+    }
+    Console.WriteLine();
+}

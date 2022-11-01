29ff9ab9fb29109414424c8677dfbd911cbd79b3
yegor3452 <ntsirius@yandex.ru>
2022-11-01 20:34:07 +0500

    48

---
+++ task48/Program.cs
@@ -0,0 +1,22 @@
+﻿// **Задача 486** Задайте двумерный массив размера m на n, каждый элемент в массиве
+// находится по формуле: Amn = m + n
+//m = 3, n = 4.
+//0 1 2 3
+//1 2 3 4
+//2 3 4 5
+
+int[,] array = new int [3, 4];
+
+for (int i = 0; i < 3; i++)
+
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = i + j;
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+
+

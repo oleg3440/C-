ab853e6e0fe122912f9c35799d59451036daef77
yegor3452 <ntsirius@yandex.ru>
2022-11-29 18:39:51 +0500

    itog3

--- itog/Program.cs
+++ itog/Program.cs
@@ -11,7 +11,7 @@
 
 
 
-string[] array1 = new string[4] {"1234", "1567", "-2", "computer science"};
+string[] array1 = new string[3] {"Russia", "Denmark", "Kazan"};
 string[] array2 = new string[array1.Length];
 void SecondArray(string[] array1, string[] array2)
 {
